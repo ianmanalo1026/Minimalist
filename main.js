c0be2ae38bbb7ae8dbc9bfa4dc68dbb41d116aef
Ian <ian.manalo1026@gmail.com>
2022-02-16 11:59:21 +0800

    constructing modals for nav bar

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let hamburger = document.querySelector(".hamburger");
+let modalBg = document.querySelector(".nav-modal-bg");
 
 // Hamburger NavBar Section
 hamburger.addEventListener("click", function () {
   this.classList.toggle("is-active");
+});
+
+hamburger.addEventListener("click", function () {
+  modalBg.classList.toggle("modal-bg-active");
 });
